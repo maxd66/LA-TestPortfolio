b3f992b80670a47854ff801d0bc81e2b36713d4d
Matt Dunston <matt.cdunston21@gmail.com>
2022-09-18 15:25:51 -0700

    bilding skeletons for contact and work js files

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,15 +10,24 @@ import Avatar from '@mui/material/Avatar';
 const About = () => {
     return (
         <>
+         <Box minHeight= '85vh'>
          <Grid justifyContent= "center"container padding={2} spacing={2}>
-            {/* <Grid item md={2}></Grid> */}
-            {/* the above grid item prevents it from being centered */}
+        {/* <Grid item md={2}></Grid> */}
+             {/* the above grid item prevents it from being centered */}
             <Grid itemxs={12} md={4}>
-            <Avatar 
-            alt="Matt Dunston" src={selfie} sx={{width: 1, height: 1}}>
-            </Avatar>
+                <Avatar 
+                    alt="Matt Dunston" src={selfie} sx={{width: 1, height: 1}}>
+                </Avatar>
             </Grid>
+
+            <Grid item md={1}></Grid>
+
+            <Grid item xs={12} md={3}>
+                <h1>Matt Dunston</h1>
+                <p> Welcome to my page, I know you won't regret checking me out. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, omnis exercitationem fuga quaerat quae eius quam numquam temporibus aliquid sapiente ipsum debitis consectetur doloribus odit, ipsam facere perferendis placeat laborum. </p>
+         </Grid>       
          </Grid>
+         </Box>
         </>
     );
 };
---
+++ src/components/Work/Work.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Card from '../Card'
+
+const Work = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Work;

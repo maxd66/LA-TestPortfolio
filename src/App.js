a0d4dcff13a3df000519af386cb171749f46890e
Matt Dunston <matt.cdunston21@gmail.com>
2022-09-18 15:56:02 -0700

    added card folder and worked on work.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Navbar from './components/Navbar'
 import CssBaseline from '@mui/material/CssBaseline';
 import About from './components/About'
-
+import Work from './components/Work'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
    <CssBaseline/>
    <Navbar />
    <About />
+   <Work/>
+   <Contact/>
    </>
   );
 }
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,12 +1,62 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import Card from '../Card'
+import Card from '../Card';
 
 const Work = () => {
+    let projects = [{
+        title: 'example',
+        github: 'https://github.com/',
+        heroku: 'https://dashbaord.heroku.com/',
+        image: 'https://images.unsplash.com/photo-1524168948265-8f79ad8d4e33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+    {
+        title: 'example',
+        github: 'https://github.com/',
+        heroku: 'https://dashbaord.heroku.com/',
+        image: 'https://images.unsplash.com/photo-1524168948265-8f79ad8d4e33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+    {
+        title: 'example',
+        github: 'https://github.com/',
+        heroku: 'https://dashbaord.heroku.com/',
+        image: 'https://images.unsplash.com/photo-1524168948265-8f79ad8d4e33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+    {
+        title: 'example',
+        github: 'https://github.com/',
+        heroku: 'https://dashbaord.heroku.com/',
+        image: 'https://images.unsplash.com/photo-1524168948265-8f79ad8d4e33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+    {
+        title: 'example',
+        github: 'https://github.com/',
+        heroku: 'https://dashbaord.heroku.com/',
+        image: 'https://images.unsplash.com/photo-1524168948265-8f79ad8d4e33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+    {
+        title: 'example',
+        github: 'https://github.com/',
+        heroku: 'https://dashbaord.heroku.com/',
+        image: 'https://images.unsplash.com/photo-1524168948265-8f79ad8d4e33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+]
+
+
+
     return (
-        <div>
-            
-        </div>
+        <>
+           <Grid container justifyContent="center">
+        <h1>My projects</h1>
+      </Grid>
+      <Grid container spacing={4} padding={2} justifyContent="center">
+        
+        {projects.map(project => 
+          <Grid item>
+            <Card title={project.title} github={project.github} heroku={project.heroku} image={project.image} ></Card>
+            </Grid>
+          )}
+        </Grid>
+        </>
     );
 };
 
